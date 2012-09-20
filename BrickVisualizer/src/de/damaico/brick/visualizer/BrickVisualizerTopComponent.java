3a742ad4e99e370c6302a5f19a92303197b53ff6
Sebastian Herbermann <sebastian.herbermann@googlemail.com>
2012-09-20 18:25:48 +0200

    add satellite view

--- BrickVisualizer/src/de/damaico/brick/visualizer/BrickVisualizerTopComponent.java
+++ BrickVisualizer/src/de/damaico/brick/visualizer/BrickVisualizerTopComponent.java
@@ -8,6 +8,7 @@ import com.tinkerforge.BrickMaster;
 import com.tinkerforge.BrickletLinearPoti;
 import com.tinkerforge.Device;
 import com.tinkerforge.IPConnection.TimeoutException;
+import java.awt.BorderLayout;
 import java.awt.Point;
 import java.awt.datatransfer.Transferable;
 import java.beans.BeanInfo;
@@ -146,6 +147,9 @@ public final class BrickVisualizerTopComponent extends TopComponent {
 
         // add it to our scrollpane
         jScrollPane1.setViewportView(scene.createView());
+        
+        jPanel1.setLayout(new BorderLayout());
+        jPanel1.add(scene.createSatelliteView(), BorderLayout.CENTER);
 
     }
 
@@ -158,19 +162,36 @@ public final class BrickVisualizerTopComponent extends TopComponent {
     private void initComponents() {
 
         jScrollPane1 = new javax.swing.JScrollPane();
+        jPanel1 = new javax.swing.JPanel();
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 51, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 400, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 300, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 300, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
     }// </editor-fold>//GEN-END:initComponents
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
     // End of variables declaration//GEN-END:variables
 
