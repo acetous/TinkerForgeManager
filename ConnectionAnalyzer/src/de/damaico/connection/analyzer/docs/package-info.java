20f5d434ddb3c01ab3c4a5f8d40cf8b9a5caf301
Sebastian Herbermann <sebastian.herbermann@googlemail.com>
2012-09-21 12:20:49 +0200

    add help for brickviewer

---
+++ ConnectionAnalyzer/src/de/damaico/connection/analyzer/docs/package-info.java
@@ -0,0 +1,8 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+@HelpSetRegistration(helpSet = "analyzer-hs.xml", position = 3576)
+package de.damaico.connection.analyzer.docs;
+
+import org.netbeans.api.javahelp.HelpSetRegistration;
